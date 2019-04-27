1f4d5dc392404eaeccb4cc7bb457286c0b8ed9b0
Kyle Warren <qarren@gmail.com>
2019-04-27 16:40:12 -0700

    started using PIXI

--- src/client.ts
+++ src/client.ts
@@ -1,28 +1,36 @@
-import {createElement} from './utils/dom.js';
+//import './webgl.js';
+//import * as PIXI from 'https://cdnjs.cloudflare.com/ajax/libs/pixi.js/4.8.7/pixi.min.js'; 
+//import * as PIXI from '../node_modules/pixi.js/dist/pixi.js'; 
+//import * as PIXI from 'pixi.js';
 
-const canvas: HTMLCanvasElement = createElement(document, 'canvas', {
-  width: window.innerWidth,
-  height: window.innerHeight,
-  style: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    background: 'black',
-  },
-});
+const app = new PIXI.Application();
+document.body.appendChild(app.view);
 
-const ctx = canvas.getContext('2d');
+const graphics = new PIXI.Graphics();
+graphics.beginFill(0xaaaaaa);
+graphics.drawRect(20, 20, 100, 100);
+graphics.endFill();
 
-ctx.fillStyle = 'blue';
-ctx.fillRect(10, 10, 20, 20);
+app.stage.addChild(graphics);
 
-document.body.appendChild(canvas);
+interface Headstone {
+  x: number,
+  y: number,
+  text: string,
+};
 
+const stones: Headstone[] = [
+  {x: 0, y: 0, text: 'Here lies Kyle'},
+];
+
+function paint() {
+}
+
+/*
 window.addEventListener('resize', () => {
   Object.assign(canvas, {
     width: window.innerWidth,
     height: window.innerHeight,
   });
 });
+*/
